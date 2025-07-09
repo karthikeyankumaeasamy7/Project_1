fd6b5efc5c8141f2cedc11544c9834bee070fedc
karthikeyankumaeasamy7 <karthikeyankumarasamy7@gmail.com>
2025-07-09 17:45:07 +0530

    Add files via upload

---
+++ MYSQL IMDB_movies data loaded.sql
@@ -0,0 +1,9 @@
+create database imdb_data;
+use imdb_data;
+create table imdb_movies;
+drop table imdb_movies;
+use imdb_data;
+use  imdb_data;
+select * from imdb_movies;
+SELECT * FROM imdb_movies LIMIT 10
+DESCRIBE imdb_movies;
